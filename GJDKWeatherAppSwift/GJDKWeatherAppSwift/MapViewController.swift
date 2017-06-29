48aa5910812a23e13cfc1bfea36385c8dca24527
Denesh <denesh.3191@gmail.com>
2017-06-29 17:52:04 +0530

    Hardcoded lat long is launched in map

--- GJDKWeatherAppSwift/GJDKWeatherAppSwift/MapViewController.swift
+++ GJDKWeatherAppSwift/GJDKWeatherAppSwift/MapViewController.swift
@@ -7,13 +7,22 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: BaseViewController {
 
+    //MARK: IBOutlets
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let annotations = MKPointAnnotation()
+        let centerCoordinate = CLLocationCoordinate2D(latitude: 9.93, longitude: 78.12)
+        annotations.coordinate = centerCoordinate
+        annotations.title = "Madurai"
+        mapView.addAnnotation(annotations)
     }
 
     override func didReceiveMemoryWarning() {

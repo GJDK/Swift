fc035044482bfe3661ef82d46419c2afba17afa9
Denesh <denesh.3191@gmail.com>
2017-07-04 11:49:00 +0530

    Drawing Route is in progress

--- GJDKWeatherAppSwift/GJDKWeatherAppSwift/MapViewController.swift
+++ GJDKWeatherAppSwift/GJDKWeatherAppSwift/MapViewController.swift
@@ -13,6 +13,9 @@ class MapViewController: BaseViewController {
     
     //MARK: Members
     var cityDetails : WeatherDetails!
+    var sourceLocation : CLLocationCoordinate2D?
+    var destinationLocation : CLLocationCoordinate2D?
+    
 
     //MARK: IBOutlets
     @IBOutlet weak var mapView: MKMapView!
@@ -21,6 +24,9 @@ class MapViewController: BaseViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        sourceLocation = CLLocationCoordinate2D(latitude: 28.67, longitude: 77.22)
+        destinationLocation = CLLocationCoordinate2D(latitude: cityDetails.latitude, longitude: cityDetails.longitude)
+        addFindDistanceButton()
         zoomToTheSelectedCity()
         addPinToTheLocation()
     }
@@ -31,8 +37,56 @@ class MapViewController: BaseViewController {
     }
     
     //MARK: Custom methods
+    func addFindDistanceButton() -> Void {
+        let rightBarButtonItem = UIBarButtonItem(title: "Find Distance", style: UIBarButtonItemStyle.plain, target: self, action: #selector(MapViewController.findDistanceButtonTapped))
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    func findDistanceButtonTapped(sender : Any) -> Void {
+        drawRoute()
+    }
+    
+    func drawRoute() -> Void {
+        let sourcePlaceMark = MKPlacemark(coordinate: sourceLocation!)
+        let destinationPlaceMark = MKPlacemark(coordinate: destinationLocation!)
+        
+        let sourceMapItem = MKMapItem(placemark: sourcePlaceMark)
+        let destinationMapItem = MKMapItem(placemark: destinationPlaceMark)
+        
+        let sourceAnnotation = MKPointAnnotation()
+        let destinationAnnotation = MKPointAnnotation()
+        sourceAnnotation.title = "Delhi - Current Location"
+        destinationAnnotation.title = cityDetails.cityName!
+        sourceAnnotation.coordinate = (sourcePlaceMark.location?.coordinate)!
+        destinationAnnotation.coordinate = (destinationPlaceMark.location?.coordinate)!
+        
+        mapView.showAnnotations([sourceAnnotation, destinationAnnotation], animated: true)
+        
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destinationMapItem
+        directionRequest.transportType = MKDirectionsTransportType.any
+        directionRequest.requestsAlternateRoutes = true;
+        let directions = MKDirections(request: directionRequest)
+        
+        directions.calculate { (response, error) in
+            guard let response = response else {
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                return
+            }
+            
+            let route = response.routes[0]
+            self.mapView.add(route.polyline, level: MKOverlayLevel.aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+        }
+    }
+    
     func zoomToTheSelectedCity() {
-        let centerCoordinate = CLLocationCoordinate2D(latitude: cityDetails.latitude, longitude: cityDetails.longitude)
+        let centerCoordinate = destinationLocation!
         let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.75, longitudeDelta: 0.75)
         let region = MKCoordinateRegion(center: centerCoordinate, span: coordinateSpan)
         mapView.setRegion(region, animated: true)
@@ -40,7 +94,7 @@ class MapViewController: BaseViewController {
     
     func addPinToTheLocation() {
         let annotations = MKPointAnnotation()
-        let centerCoordinate = CLLocationCoordinate2D(latitude: cityDetails.latitude, longitude: cityDetails.longitude)
+        let centerCoordinate = destinationLocation!
         annotations.coordinate = centerCoordinate
         annotations.title = cityDetails.cityName!
         annotations.subtitle = cityDetails.temperature!
@@ -77,5 +131,12 @@ extension MapViewController : MKMapViewDelegate {
             annotationView.image = UIImage(named: "LighteningIcon")
         }
         return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.red
+        renderer.lineWidth = 4.0
+        return renderer
     }
 }
